1a725b575c2276c2eeda7bb9393f2f2f845068d5
t-pedrohenrique-iabs <t-pedro.henrique@iabs.org.br>
2022-07-25 17:55:14 -0300

    first commit

--- djangojenkins/settings.py
+++ djangojenkins/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django
 from pathlib import Path
+from django.utils.encoding import smart_str 
+
+django.utils.encoding.smart_text = smart_str
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
